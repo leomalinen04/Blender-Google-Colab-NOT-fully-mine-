ad67af0052e2d2749b96ebaab087751c0cfadf82
Leo Malinen <107366630+leomalinen04@users.noreply.github.com>
2024-05-27 13:32:19 -0700

    Update notebook+code.py

--- notebook+code.py
+++ notebook+code.py
@@ -1,6 +1,9 @@
 #check the specs of what you have (be sure you enable the GPU in the edit configuration in the notebook)
 ! nvidia-smi
 
+#be sure to make the directory for where to store the image sequence below
+mkdir Image_Sequence
+
 #updated code for blender usage with the latest blender release (change the blend file to whatever the name of your project is)
 #Step-1: Connect Google Drive to colab
 from google.colab import drive
@@ -13,8 +16,8 @@ drive.mount('/content/drive')
 !rm -rf "./Image Sequence"
 
 #Step-4: Render the animation on colab
-! ./blender-4.1.1-linux-x64/blender -b -P "/content/drive/My Drive/Colab-Render/enable_gpu.py" "/content/drive/My Drive/Colab-Render/Example.blend" -noaudio -F PNG -E CYCLES -o "/content/Image Sequence/" -f 1
+! ./blender-4.1.1-linux-x64/blender -b -P "/content/drive/My Drive/Colab-Render/enable_gpu.py" "/content/drive/My Drive/Colab-Render/LCorpJet.blend" -noaudio -F PNG -E CYCLES -o "/content/Image_Sequence/" -f 1
 
 #Step-5: Copy output files from colab to drive
 import shutil
-shutil.copytree("/content/Image Sequence", "/content/drive/My Drive/Colab-Render/Image Sequence", dirs_exist_ok=True)
+shutil.copytree("/content/Image_Sequence", "/content/drive/My Drive/Colab-Render/Image Sequence", dirs_exist_ok=True)
